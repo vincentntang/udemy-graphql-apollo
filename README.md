db2ee0654806c0d39c82fc457dd6afcc5e1fe518
vincenttang <10290348+vincentntang@users.noreply.github.com>
2019-09-10 15:11:16 -0400

    finish section 1 of app

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 Notes here
 
-npm install graphql graphql-tools apollo-server-expres
+npm install graphql graphql-tools apollo-server-express@1
 
 1. When using version 2.0, the graphQL express is not treated as a function
 
+```
 node server.js
-
 cd hello-world-client && npm run start
+```
--- hello-world-client/src/App.js
+++ hello-world-client/src/App.js
@@ -23,19 +23,7 @@ export default class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          {greeting}
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {greeting} 
         </header>
       </div>
     )
